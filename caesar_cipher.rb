c4266b674af6de8661c8b83d6a447a9d254497b8
Curtis <excentriceevee@hotmail.com>
2020-05-05 05:50:31 -0400

    Initial commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,33 @@
+print "Type a phrase you wish to encrypt: "
+phrase = gets.chomp
+
+print "Enter a number for the shift factor: "
+shift_factor = gets.chomp.to_i
+
+def encrypt(phrase, shift_factor)
+  final = ""
+
+  #break each char into codepoints
+  phrase.each_codepoint do |c|
+    if (c.chr.match(/[A-Z]/))
+      c += shift_factor
+      #Z is codepoint 90...
+      if (c > 90)
+        #...so loop back to A
+        c -= 26
+      end
+    elsif (c.chr.match(/[a-z]/))
+      c += shift_factor
+      #same as above but for downcase
+      if (c > 122)
+        c -= 26
+      end
+    end
+  
+    #convert back to char and add onto final variable
+    final += c.chr
+  end
+  return final
+end
+
+puts encrypt(phrase, shift_factor)

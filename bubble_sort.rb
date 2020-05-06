68f6f7a1c2b6e1dc05cbf3830a5377355c7f7ed0
Curtis <excentriceevee@hotmail.com>
2020-05-06 04:06:47 -0400

    Updated to terminate loops early if the array is already sorted

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,23 +2,34 @@ require 'pry'
 
 def bubble_sort(list)
   temp = 0
+  change_made = false
+  #to test how many passes are made, for efficiency
+  count = 0
 
   #pass through array number of times equal to number of items minus one
   (list.length-1).times do
-    #a pass through of value checks
-    list.each_with_index do |value, idx|
+    change_made = false
+    count += 1
 
+    #a passthrough of value checks
+    list.each_with_index do |value, idx|
       unless (idx+1 == list.length)
         if (value > list[idx+1])
           temp = value
           list[idx] = list[idx+1]
           list[idx+1] = temp
+          change_made = true
         end
       end
     end
+
+    #exit loop if this passthrough did nothing; it's already sorted
+    break if(change_made == false)
   end
 
   p list
+  p count
 end
 
 bubble_sort([4,3,78,2,0,2])
+bubble_sort([1,2,3,5,4,6,7,8,9])
